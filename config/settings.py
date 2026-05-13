a9c13f4c4f59914e65d95fa2458f028dd98dd7a8
CamiloPosada <camiloposada1005@gmail.com>
2026-05-12 23:38:23 -0500

    add apps in setting

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'businesses',
+    'dashboard',
+    'purchases',
+    'cash_closing',
+    'reports',
 ]
 
 MIDDLEWARE = [
