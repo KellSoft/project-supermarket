139eb7816f8a06dd4f2a0da90ecbf8f617918788
CamiloPosada <camiloposada1005@gmail.com>
2026-05-20 20:52:44 -0500

    feat: Implement dashboard module with views, templates, and sidebar navigation

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.dashboard.urls')),
+     path('', include('apps.dashboard.urls', namespace='dashboard')),
     path('accounts/', include('apps.accounts.urls')),
     path('cash-closing/', include('apps.cash_closing.urls')),
     path('purchases/', include('apps.purchases.urls')),
