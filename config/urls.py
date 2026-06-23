8e174ae93b329fc652ff5f5224cd99cfad1e2632
KellSoft <kellyaramburo8@gmail.com>
2026-06-23 13:53:41 -0500

    fix: favicon

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,4 +10,5 @@ urlpatterns = [
     path("flujo-caja/", include("apps.cash_closing.urls")),
     path("compras/", include("apps.purchases.urls")),
     path("reportes/", include("apps.reports.urls")),
+    path("favicon.ico", RedirectView.as_view(url="/static/mana-logo.jpeg")),
 ]
